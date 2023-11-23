b5639044d965080c919018860ef77458d29ad922
Selenmon <jakubspisak79@gmail.com>
2023-11-23 11:43:10 +0100

    Seeder pre tabulku Person pomocou Faker library

--- RestAPI/src/main/java/SoftveroveInzinierstvoTim5/RestAPI/controller/RestCallController.java
+++ RestAPI/src/main/java/SoftveroveInzinierstvoTim5/RestAPI/controller/RestCallController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.github.javafaker.Faker;
+
 import SoftveroveInzinierstvoTim5.RestAPI.dto.PersonDTO;
 import SoftveroveInzinierstvoTim5.RestAPI.model.Person;
 import SoftveroveInzinierstvoTim5.RestAPI.service.DefaultPersonService;
@@ -17,16 +19,36 @@ public class RestCallController {
 
     @GetMapping("/test")
     public String handleTestRequest() {
-        PersonDTO p = new PersonDTO();
-        p.setId_person(0);
-        p.setName("name");
-        personService.savePerson(p);
         List <PersonDTO> persons = personService.getAllPersons();
-        String firstPerson = persons.get(0).getName();
-        return firstPerson;
+        String people = "";
+        for (PersonDTO personDTO : persons) {
+            people += "Person: " + personDTO.getName() + " "+ personDTO.getSurname() + 
+            " Email: " + personDTO.getEmail() + " Adress: "+ personDTO.getAddress() + "\n";
+        }
+        return people;
     }
     @GetMapping("/dataSeed")
     public String handleDataSeedRequest(){
+    for (int i = 0; i < 20; i++) {
+        PersonDTO p = new PersonDTO();
+        Faker faker = new Faker();
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+
+        String streetAddress = faker.address().streetAddress();
+        p.setEmail(firstName+lastName+"@gmail.com");
+        p.setName(firstName);
+        p.setSurname(lastName);
+        p.setAddress(streetAddress);
+        personService.savePerson(p);
+    }
         return "Data Loaded";
     }
+    @GetMapping("/login")
+    public String handleLoginRequest(){
+        
+        return "Login Successful";
+    }
+
+    
 }
