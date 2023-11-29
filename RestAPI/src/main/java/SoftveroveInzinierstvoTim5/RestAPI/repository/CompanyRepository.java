cb5e29fe998302ecfd811c04edbba26782b26eda
Petino01 <syran12@gmail.com>
2023-11-29 08:43:22 +0100

    JPA repo a Service pre Company model

--- RestAPI/src/main/java/SoftveroveInzinierstvoTim5/RestAPI/repository/CompanyRepository.java
+++ RestAPI/src/main/java/SoftveroveInzinierstvoTim5/RestAPI/repository/CompanyRepository.java
@@ -1,5 +1,11 @@
 package SoftveroveInzinierstvoTim5.RestAPI.repository;
 
-public class CompanyRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import SoftveroveInzinierstvoTim5.RestAPI.model.Company;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company,Integer>{
     
-}
+} 
