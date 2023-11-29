48fa7fdc1463aae572e2a66fa128f26941f5754b
Petino01 <syran12@gmail.com>
2023-11-29 09:50:35 +0100

    JPA repo a Service pre Subject for Practice model

---
+++ RestAPI/src/main/java/SoftveroveInzinierstvoTim5/RestAPI/repository/Subject_For_PracticeRepository.java
@@ -0,0 +1,9 @@
+package SoftveroveInzinierstvoTim5.RestAPI.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import SoftveroveInzinierstvoTim5.RestAPI.model.Subject_for_Practice;
+
+public interface Subject_For_PracticeRepository extends JpaRepository<Subject_for_Practice, Integer>{
+    
+}
---
+++ RestAPI/src/main/java/SoftveroveInzinierstvoTim5/RestAPI/service/DefaultSubject_For_PracticeService.java
@@ -0,0 +1,61 @@
+package SoftveroveInzinierstvoTim5.RestAPI.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import SoftveroveInzinierstvoTim5.RestAPI.dto.Subject_For_PracticeDTO;
+import SoftveroveInzinierstvoTim5.RestAPI.model.Subject_for_Practice;
+import SoftveroveInzinierstvoTim5.RestAPI.repository.Subject_For_PracticeRepository;
+import jakarta.persistence.EntityNotFoundException;
+
+public class DefaultSubject_For_PracticeService implements Subject_For_PracticeService{
+
+    @Autowired
+    Subject_For_PracticeRepository subject_For_PracticeRepository;
+
+    @Override
+    public Subject_For_PracticeDTO saveSubjectForPractice(Subject_For_PracticeDTO subject_for_practice) {
+        Subject_for_Practice subjectForPracticeModel = populateSubjectForPracticeEntity(subject_for_practice);
+        return populateSubjectForPracticeData(subject_For_PracticeRepository.save(subjectForPracticeModel));
+    }
+
+    @Override
+    public boolean deleteSubjectForPractice(Integer idsubject_for_practice) {
+        subject_For_PracticeRepository.deleteById(idsubject_for_practice);
+        return true;
+    }
+
+    @Override
+    public List<Subject_For_PracticeDTO> getAllSubjectsForPractice() {
+        List<Subject_For_PracticeDTO> subjectsForPractice = new ArrayList<>();
+        Iterable<Subject_for_Practice> subjectForPracticeList = subject_For_PracticeRepository.findAll();
+        subjectForPracticeList.forEach(subject_for_practice -> {
+            subjectsForPractice.add(populateSubjectForPracticeData(subject_for_practice));
+        });
+        return subjectsForPractice;
+    }
+
+    @Override
+    public Subject_For_PracticeDTO getSubjectForPracticeById(Integer idsubject_for_practice) {
+        return populateSubjectForPracticeData(subject_For_PracticeRepository.findById(idsubject_for_practice).orElseThrow(() -> new EntityNotFoundException("Subject for Practice not found")));
+    }
+
+    private Subject_For_PracticeDTO populateSubjectForPracticeData (final Subject_for_Practice subject_for_practice) {
+        Subject_For_PracticeDTO subjectForPracticeData = new Subject_For_PracticeDTO();
+        subjectForPracticeData.setStudy_program_idstudy_program(subject_for_practice.getStudy_program_idstudy_program());
+        subjectForPracticeData.setName(subject_for_practice.getName());
+        subjectForPracticeData.setCredits(subject_for_practice.getCredits());
+        return subjectForPracticeData;
+    }
+
+    private Subject_for_Practice populateSubjectForPracticeEntity(Subject_For_PracticeDTO subjectForPracticeData) {
+        Subject_for_Practice subject_for_practice = new Subject_for_Practice();
+        subject_for_practice.setStudy_program_idstudy_program(subjectForPracticeData.getStudy_program_idstudy_program());
+        subject_for_practice.setName(subjectForPracticeData.getName());
+        subject_for_practice.setCredits(subjectForPracticeData.getCredits());
+        return subject_for_practice;
+    }
+    
+}
